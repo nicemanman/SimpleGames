805c7a8a5a39de8608443dc8f7f49cf972bc959e
niceman <ilya.falko2013@gmail.com>
2020-03-13 10:26:27 +0300

    Tiled2Unity, Комната в "Tiled"

---
+++ Assets/ZeldaLike/Assets/TiledMaps/Overworld..tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../gfx/gfx/Overworld.png" width="640" height="576"/>
+ <terraintypes>
+  <terrain name="Path" tile="-1"/>
+  <terrain name="forest" tile="1201"/>
+ </terraintypes>
+ <tile id="685" terrain=",1,,"/>
+ <tile id="686" terrain="1,,,"/>
+ <tile id="763" terrain=",1,,"/>
+ <tile id="764" terrain="1,1,,"/>
+ <tile id="765" terrain="1,,,"/>
+ <tile id="1160" terrain=",,,0"/>
+ <tile id="1161" terrain=",,0,0"/>
+ <tile id="1162" terrain=",,0,"/>
+ <tile id="1200" terrain=",0,,0"/>
+ <tile id="1201" terrain="0,0,0,0"/>
+ <tile id="1202" terrain="0,,0,"/>
+ <tile id="1240" terrain=",0,,"/>
+ <tile id="1241" terrain="0,0,,"/>
+ <tile id="1242" terrain="0,,,"/>
+ <tile id="1280" terrain="0,0,0,"/>
+ <tile id="1281" terrain="0,0,,0"/>
+ <tile id="1320" terrain="0,,0,0"/>
+ <tile id="1321" terrain=",0,0,0"/>
+</tileset>
