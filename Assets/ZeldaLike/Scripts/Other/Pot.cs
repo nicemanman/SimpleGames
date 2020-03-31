dea788d844557a69f4b01b682d2bc436dfd204e0
Ilya Falko <ilya.falko2013@gmail.com>
2020-04-01 02:15:50 +0700

    Breakable pots!:)

---
+++ Assets/ZeldaLike/Scripts/Other/Pot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Pot : MonoBehaviour, Smashable
+{
+    [SerializeField] private Animator animator;
+    public void Smash()
+    {
+        animator?.SetBool("GotDamage", true);
+        StartCoroutine(waitAndDestroy());
+    }
+
+    private IEnumerator waitAndDestroy() 
+    {
+        yield return new WaitForSeconds(.2f);
+        Destroy(gameObject);
+    }
+    public void Awake()
+    {
+        tag = "Smashable";
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hurtable")) 
+        {
+            Smash();
+        }
+    }
+}
